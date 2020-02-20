9c041238cf829c471fee47ea4404325bc3582b37
Josh Wulf <josh@magikcraft.io>
2020-02-21 06:23:47 +1000

    Create cloud-workers.js

---
+++ cloud-workers.js
@@ -0,0 +1,25 @@
+
+
+
+ module.exports = {
+  tasks: {
+  "ImageInspection", (job, complete) => {
+        console.log(job.variables);
+        CompleteImageInspectionWorker(complete);
+    }),
+
+    "VideoInspection", (job, complete) => {
+        console.log(job.variables);
+        CompleteVideoInspectionWorker(complete);
+    }),
+  }
+}
+    function CompleteImageInspectionWorker(complete) {
+        complete.success({ outFrameLocation: 'up your ass', });
+    }
+
+    function CompleteVideoInspectionWorker(complete) {
+        complete.success({ baseOutputImageLocation: 'C:/bob', videoFrames: ['c:/frame1', 'c:/frame2'] });
+    }
+
+
